05c064196bb85f458861adeb78981298f785d123
norbs <norbertbobila12@gmail.com>
2022-12-13 13:45:24 +0800

    generate 1000 firstname and lastname for batchInsert

--- src/main/java/sakila_main/SakilaApplication.java
+++ src/main/java/sakila_main/SakilaApplication.java
@@ -7,6 +7,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SakilaApplication {
     public static void main(String[] args) {
         System.out.println("Norbs Project");
+        /*String firstname="";
+        String lastname="";
+        for (int i = 1; i <= 1000; i++) {
+            firstname= firstname + "sku,"+i;
+        }
+        for (int i = 1; i <= 1000; i++) {
+            lastname= lastname +"bku,"+i;
+        }
+        firstname = firstname.trim();
+        lastname = lastname.trim();
+        System.out.print(firstname);
+        System.out.println("\n");
+        System.out.print(lastname);*/
         SpringApplication.run(SakilaApplication.class, args);
+
+
+
+
     }
 }
